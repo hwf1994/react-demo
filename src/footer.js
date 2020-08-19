2e4f442c0d3c29018bcc9ae95f9218500f199f2f
何文飞 <hewenfei@yolanda.hk>
2020-08-19 19:41:05 +0800

    修改文件

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+         }
+    }
+    render() { 
+        return ( 
+            <p>返回默认组件</p>
+         );
+    }
+}
+ 
+export default Footer;
